6e4fce2f4313e5e79c89c968f13d8a94f433fdc3
root <root@dev-amos2.dev-amos2.c2.internal.cloudapp.net>
2015-07-30 12:50:44 +0000

    change footer

--- footer.php
+++ footer.php
@@ -3,6 +3,6 @@
 <script id="cid0020000099200626419" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 250px;height: 350px;">{"handle":"healthbuddies","arch":"js","styles":{"a":"ff6600","b":52,"c":"000000","d":"000000","f":52,"i":52,"k":"ff6600","l":"ff6600","m":"ff6600","o":52,"p":"10","q":"ff6600","r":52,"fwtickm":1}}</script>
 </p>
 <p>
-		<a href='aboutus.php'>About Us</a> | <a href='#'> FAQ</a> | <a href='contact.php'> Contact</a> | <a href='termsofservice.php'> Terms of Service</a>
+		<a href='aboutus.php'>About Us</a> | <a href='contact.php'> Contact</a> | <a href='termsofservice.php'> Terms of Service</a>
 </p>
 </div>
